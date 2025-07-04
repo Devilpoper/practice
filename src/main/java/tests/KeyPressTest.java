7aec6f4efa7aba3f1139a5f48f513bb9cdabbc54
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:37:24 +0300

    KeyPressTest.java

--- src/main/java/tests/KeyPressTest.java
+++ src/main/java/tests/KeyPressTest.java
@@ -7,17 +7,26 @@ import org.openqa.selenium.Keys;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Проверяет корректное отображение текста при нажатии различных клавиш.
+ */
 public class KeyPressTest extends BaseTest {
 
+    /**
+     * Проверяет отображение текста для обычной клавиши и клавиши Enter.
+     */
     @Test
     public void testKeyPresses() {
+        // Открываем страницу с обработкой нажатий клавиш
         driver.get("https://the-internet.herokuapp.com/key_presses");
 
         KeyPressPage keyPressPage = new KeyPressPage(driver);
 
+        // Нажимаем клавишу "g" и проверяем отображение результата
         keyPressPage.pressKey("g");
         assertEquals("You entered: G", keyPressPage.getDisplayedText(), "Клавиша G не отображается корректно");
 
+        // Нажимаем клавишу ENTER и проверяем отображение результата
         keyPressPage.pressKey(Keys.ENTER);
         assertEquals("You entered: ENTER", keyPressPage.getDisplayedText(), "Клавиша ENTER не отображается корректно");
     }
