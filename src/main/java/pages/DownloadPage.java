7c709319b2bcf5d71984a5ea1f78f27ed4ca72e7
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:11:42 +0300

    DownloadPage.java

--- src/main/java/pages/DownloadPage.java
+++ src/main/java/pages/DownloadPage.java
@@ -4,12 +4,21 @@ import base.BasePage;
 import elements.DownloadLink;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Страница со ссылками для загрузки файлов.
+ */
 public class DownloadPage extends BasePage {
 
     public DownloadPage(WebDriver driver) {
         super(driver, "download");
     }
 
+    /**
+     * Получает ссылку на файл по его имени.
+     *
+     * @param fileName имя файла, как указано в href
+     * @return объект DownloadLink для взаимодействия
+     */
     public DownloadLink getDownloadLink(String fileName) {
         return new DownloadLink(driver, fileName);
     }
