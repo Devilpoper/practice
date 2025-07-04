1500bc9807d0787919698f25c16fae0634cf45df
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:26:24 +0300

    ContextMenuTest.java

--- src/main/java/tests/ContextMenuTest.java
+++ src/main/java/tests/ContextMenuTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pages.ContextMenuPage;
 
+/**
+ * Тест проверки появления alert после клика правой кнопкой на hotspot.
+ */
 public class ContextMenuTest extends BaseTest {
 
     @Test
@@ -12,11 +15,14 @@ public class ContextMenuTest extends BaseTest {
         driver.get("https://the-internet.herokuapp.com/context_menu");
         ContextMenuPage page = new ContextMenuPage(driver);
 
+        // Выполняем правый клик на указанном элементе
         page.rightClickOnHotspot();
 
+        // Проверяем текст alert
         String alertText = page.getAlertText();
         Assertions.assertEquals("You selected a context menu", alertText, "Некорректный текст alert");
 
+        // Закрываем alert
         page.closeAlert();
     }
 }
