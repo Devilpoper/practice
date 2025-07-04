616914861894b644fd3907de27e45b79902bbe94
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 20:49:43 +0300

    DownloadLink.java

--- src/main/java/elements/DownloadLink.java
+++ src/main/java/elements/DownloadLink.java
@@ -1,14 +1,21 @@
-package elements;
-
-import base.BaseElement;
-import org.openqa.selenium.WebDriver;
-
+/**
+ * Класс для работы со ссылками на скачивание файлов.
+ */
 public class DownloadLink extends BaseElement {
 
+    /**
+     * Конструктор ссылки на скачивание по части имени файла.
+     *
+     * @param driver   экземпляр WebDriver
+     * @param fileName часть имени файла в href
+     */
     public DownloadLink(WebDriver driver, String fileName) {
         super(driver, "//a[contains(@href,'%s')]", fileName);
     }
 
+    /**
+     * Кликает по ссылке на скачивание.
+     */
     public void click() {
         driver.findElement(locator).click();
     }
