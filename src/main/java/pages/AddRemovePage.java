0f54aaace600bcc70f1f74e9ad1bcba8efeade90
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:10:08 +0300

    AddRemovePage.java

--- src/main/java/pages/AddRemovePage.java
+++ src/main/java/pages/AddRemovePage.java
@@ -9,43 +9,43 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 /**
- * Страница "Add/Remove Elements" без прямых By:
- * все действия через класс Button.
+ * Страница "Add/Remove Elements".
+ * Использует элементы типа Button для взаимодействия.
  */
 public class AddRemovePage extends BasePage {
 
+    // Кнопка "Add Element"
     private final Button addElementButton;
 
-
     public AddRemovePage(WebDriver driver) {
-        // В качестве уникального признака страницы можно взять, например, текст кнопки
         super(driver, "elements");
         this.addElementButton = Button.byXPath(driver, "/html/body/div[2]/div/div/button");
     }
 
-    /** Функция нажатия на кнопку */
+    /** Нажимает на кнопку "Add Element" */
     public void clickAddElement() {
         addElementButton.click();
     }
 
-    public List<WebElement> getDeleteButtons(){
+    /** Возвращает список всех текущих кнопок "Delete" */
+    public List<WebElement> getDeleteButtons() {
         WebElement container = driver.findElement(By.xpath("/html/body/div[2]/div/div/div"));
         return container.findElements(By.tagName("button"));
     }
 
-    /** Возвращает текущее число кнопок Delete */
+    /** Возвращает количество кнопок "Delete" */
     public int getDeleteButtonsCount() {
         return getDeleteButtons().size();
     }
 
-    /** Удаляет все кнопки Delete, кликая по первой из них до тех пор, пока они есть */
+    /** Удаляет все элементы, поочерёдно кликая по каждой кнопке "Delete" */
     public void deleteAllElements() {
         for (WebElement element : getDeleteButtons()) {
             element.click();
         }
     }
 
-    /** Проверяет, что на странице есть хотя бы одна Delete-кнопка */
+    /** Проверяет наличие хотя бы одной кнопки "Delete" */
     public boolean hasDeleteButtons() {
         return getDeleteButtonsCount() > 0;
     }
