2c721d8508c4501ea585d5650d27f51358c9e81e
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:18:41 +0300

    InputsPage.java

--- src/main/java/pages/InputsPage.java
+++ src/main/java/pages/InputsPage.java
@@ -4,7 +4,12 @@ import base.BasePage;
 import elements.Input;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Страница с числовым полем ввода.
+ */
 public class InputsPage extends BasePage {
+
+    // Input — поле ввода. В данном случае используется для чисел.
     public final Input numberInput;
 
     public InputsPage(WebDriver driver) {
@@ -12,6 +17,9 @@ public class InputsPage extends BasePage {
         this.numberInput = new Input(driver, "/html/body/div[2]/div/div/div/div/input", "");
     }
 
+    /**
+     * Переход на URL страницы с вводом чисел.
+     */
     public void open() {
         driver.get("https://the-internet.herokuapp.com/inputs");
     }
