7c2c2568a331df5247747b1db11bcea2251ffb35
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 20:56:49 +0300

    ContextArea.java

--- src/main/java/elements/ContextArea.java
+++ src/main/java/elements/ContextArea.java
@@ -1,11 +1,17 @@
+package elements;
+
+import base.BaseElement;
+import org.openqa.selenium.WebDriver;
+
 /**
- * Класс для работы с контекстной областью (например, зона для правого клика).
+ * Класс для работы с контекстной областью на странице.
+ * Используется, например, для тестирования правого клика.
  */
 public class ContextArea extends BaseElement {
     public static final String HOTSPOT_XPATH = "/html/body/div[2]/div/div/div";
 
     /**
-     * Конструктор элемента контекстной области.
+     * Конструктор контекстной области с фиксированным XPath.
      *
      * @param driver экземпляр WebDriver
      */
