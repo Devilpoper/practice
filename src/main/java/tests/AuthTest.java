d394a010759b392de0f90cdac59482f9f5c3c83e
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:23:58 +0300

    AuthTest.java

--- src/main/java/tests/AuthTest.java
+++ src/main/java/tests/AuthTest.java
@@ -1,12 +1,14 @@
 package tests;
 
-
 import base.BaseTest;
 import org.junit.jupiter.api.Test;
 import pages.AuthPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Тест авторизации по базовой аутентификации (basic auth).
+ */
 public class AuthTest extends BaseTest {
 
     @Test
@@ -14,6 +16,7 @@ public class AuthTest extends BaseTest {
         driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
 
         AuthPage page = new AuthPage(driver);
+
         String expected = "Congratulations! You must have the proper credentials.";
         assertEquals(expected, page.getMessage(), "Сообщение после авторизации не соответствует ожидаемому");
     }
