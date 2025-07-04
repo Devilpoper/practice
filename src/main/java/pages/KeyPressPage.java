aff0c46c8096d2a9d34e025c4a68442d335a81ae
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:19:18 +0300

    KeyPressPage.java

--- src/main/java/pages/KeyPressPage.java
+++ src/main/java/pages/KeyPressPage.java
@@ -6,8 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+/**
+ * Страница для проверки ввода с клавиатуры.
+ */
 public class KeyPressPage extends BasePage {
+
+    // Используем Actions для отправки нажатий клавиш
     private final Actions actions;
+
+    // Локатор результата отображаемого текста
     private final By resultText = By.xpath("/html/body/div[2]/div/div/p[2]");
 
     public KeyPressPage(WebDriver driver) {
@@ -15,10 +22,16 @@ public class KeyPressPage extends BasePage {
         this.actions = new Actions(driver);
     }
 
+    /**
+     * Отправляет нажатие клавиши на страницу.
+     */
     public void pressKey(CharSequence key) {
         actions.sendKeys(key).perform();
     }
 
+    /**
+     * Получает текст, который появился после нажатия клавиши.
+     */
     public String getDisplayedText() {
         WebElement resultElement = driver.findElement(resultText);
         return resultElement.getText();
