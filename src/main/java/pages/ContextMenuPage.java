7ec8e4bf795246bddd7921bde4882ba74bf95ef5
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:11:14 +0300

    ContextMenuPage.java

--- src/main/java/pages/ContextMenuPage.java
+++ src/main/java/pages/ContextMenuPage.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 
+/**
+ * Страница с контекстным меню.
+ * Реализует правый клик и работу с алертом.
+ */
 public class ContextMenuPage extends BasePage {
 
     private final ContextArea contextArea;
@@ -16,17 +20,20 @@ public class ContextMenuPage extends BasePage {
         this.contextArea = new ContextArea(driver);
     }
 
+    /** Выполняет правый клик на выделенной области (hotspot) */
     public void rightClickOnHotspot() {
         new Actions(driver)
                 .contextClick(driver.findElement(By.xpath(contextArea.HOTSPOT_XPATH)))
                 .perform();
     }
 
+    /** Получает текст всплывающего окна (alert) */
     public String getAlertText() {
         Alert alert = driver.switchTo().alert();
         return alert.getText();
     }
 
+    /** Закрывает alert (нажимает OK) */
     public void closeAlert() {
         driver.switchTo().alert().accept();
     }
