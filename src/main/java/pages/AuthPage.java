6abfb4d809d4c7b5323b31437f0d912efd4d6c2b
Перевалов Павел <145105449+PerevalovPavel3341@users.noreply.github.com>
2025-07-04 21:10:28 +0300

    AuthPage.java

--- src/main/java/pages/AuthPage.java
+++ src/main/java/pages/AuthPage.java
@@ -1,11 +1,15 @@
 package pages;
 
-
 import base.BasePage;
 import elements.Label;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Страница успешной авторизации.
+ * Проверяет наличие поздравительного сообщения.
+ */
 public class AuthPage extends BasePage {
+
     private final Label successMessage;
     private static final String UNIQUE_TEXT = "Congratulations!";
 
@@ -14,6 +18,7 @@ public class AuthPage extends BasePage {
         this.successMessage = Label.byPartialText(driver, UNIQUE_TEXT);
     }
 
+    /** Получает текст сообщения об успешной авторизации */
     public String getMessage() {
         return successMessage.getText();
     }
